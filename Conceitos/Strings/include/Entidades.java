69a96927f81cc7d9d634055e5854a3e9c667ff75
LuanJefL <luanjerfessonlopes2016@gmail.com>
2026-08-31 16:30:16 -0300

    Estruturando pastas

---
+++ Conceitos/Strings/include/Entidades.java
@@ -0,0 +1,20 @@
+package include;
+
+public class Entidades{
+
+    public String nome;
+
+    public Entidades(String nome) {
+
+        this.nome = nome;
+
+    }
+
+    public void imprimir() {
+
+        System.out.println(nome);
+
+    }
+
+
+}
---
+++ Conceitos/Strings/src/Main.java
@@ -0,0 +1,12 @@
+import include.*;
+
+public class Main{
+    public static void main(String[] args) {
+
+        Entidades Julio = new Entidades("tonbeoitbneotib");
+
+        Julio.imprimir();
+
+    }
+
+}
